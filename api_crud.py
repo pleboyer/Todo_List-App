380d5b430e6eb524560a1fd0aa8dc801bb118073
Paul Leboyer <paul.leboyer@cirruseo.com>
2019-09-20 14:46:52 +0200

    Add early beginning of DELETE and PUT

--- api_crud.py
+++ api_crud.py
@@ -17,7 +17,7 @@ todos = db.todo
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST', "PUT", "DELETE"])
 def get_all_contact():
 	if request.method == 'GET':
 		todo_list = todos.find()
@@ -37,9 +37,23 @@ def get_all_contact():
 
 		result = todos.insert(todo_line)
 		print('One post: {0}'.format(result))
-
 		return dumps({'message': 'SUCCESS'})
 
+	#TODO
+	"""if request.method == 'PUT':
+		data = json.loads(request.data)
+
+		todo_line = {
+			'message': data['message']
+		}
+
+		todo_update = todos.find(id).update(todo_line)
+		return dumps({'message': 'SUCCESS'})"""
+
+	#TODO
+	"""if request.method == 'DELETE':
+		todo_delete = todos.find(id).remove()
+		return dumps({'message': 'SUCCESS'})"""
 
 if __name__ == '__main__':
     app.run()
