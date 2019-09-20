b2643cbc2fbf05dc2d1e02a03a7eda72484d7033
Paul Leboyer <paul.leboyer@cirruseo.com>
2019-09-20 14:27:27 +0200

    Beginning the API, testing things with GET and POST methods

---
+++ api_crud.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+__author__ = 'admin'
+
+from flask import Flask
+from flask import request, jsonify
+from pymongo import MongoClient
+from bson.json_util import dumps
+import json
+
+client = MongoClient('localhost:27017')
+db = client.ToDo
+todos = db.todo
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def get_all_contact():
+	if request.method == 'GET':
+		todo_list = todos.find()
+		todo_content = []
+		for element in todo_list:
+			print("element", element)
+			todo_content.append(dumps(element["message"]))
+			print("todo_content", todo_content)
+		return jsonify(todo_content)
+
+	if request.method == 'POST':
+		data = json.loads(request.data)
+
+		todo_line = {
+			'message': data['message']
+		}
+
+		result = todos.insert(todo_line)
+		print('One post: {0}'.format(result))
+
+		return dumps({'message': 'SUCCESS'})
+
+
+if __name__ == '__main__':
+    app.run()
