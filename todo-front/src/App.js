63d1f0ee9cb799d4d0a39481e6d62897f3664c93
pleboyer <paul.leboyer@gmail.com>
2019-10-24 15:57:22 +0200

    Modifying App and ToDoList to show the list of task on screen

--- todo-front/src/App.js
+++ todo-front/src/App.js
@@ -13,20 +13,16 @@ class App extends Component {
 
   addItem(todoItem) {
     console.log('Should print todoItem : ', todoItem);
-    //var newItems = []
-    //newItems.push({value: todoItem.newItem});
-    //console.log('Should print newItems : ', newItems);
     this.setState({
       itemsList: [...this.state.itemsList, todoItem.newItem]
     });
-    console.log('Should print itemList : ', this.state.itemsList);
   }
 
   render() {
     return (
       <div className="App">
         <ToDoInput addItem = {this.addItem}/>
-        {this.state.itemsList}
+        <ToDoList itemsList={this.state.itemsList}/>
       </div>
     )
   }
--- todo-front/src/ToDoList.js
+++ todo-front/src/ToDoList.js
@@ -4,15 +4,19 @@ import ToDoItem from './ToDoItem'
 class ToDoList extends Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
+    var items = this.props.itemsList.map((item)=>{
+      return <li>{item}</li>
+    })
+    console.log('ICI', items);
     return (
-        <ul className='todo-list'>
-          <ToDoItem/>
-        </ul>
-    )
-  }
+      <ul className='todo-list'>
+        {items}
+      </ul>
+  )}
 }
 
 export default ToDoList
